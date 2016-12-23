079aa0c0926c9214dcf9fdaa00af17fd3d38ca27
Wafa Kader <wafakader@ymail.com>
2016-12-23 15:15:57 +0100

    Update Fib1.py

--- Fib1.py
+++ Fib1.py
@@ -21,6 +21,6 @@ f=Fibonacci(13)
 k=f.run()
 print(k)
 
-# Pour calculer Fibonacci(13), on commence par tester : 13<>0 et 13<>1 alors on crée la liste u 
+# Pour calculer Fibonacci(13), on commence par tester : 13<>0 et 13<>1 alors on crée la liste u pour enregistrer les éléments u[i]
 # il faut calculer Fibonacci(2) et Fibonacci(3) ... jusqu'à arriver à Fibonacci(13)
 # on affiche alors l'élément n-1 de la liste u qui correspond au résultat demandé
