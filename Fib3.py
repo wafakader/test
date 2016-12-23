86a1ed84d9294f81d5a08112bce2b2f8dca872de
Wafa Kader <wafakader@ymail.com>
2016-12-23 15:21:38 +0100

    Update Fib3.py

--- Fib3.py
+++ Fib3.py
@@ -15,10 +15,15 @@ class Fibonacci:
                 print ("Fibonacci(",i,")= ",x)
                 b=a
                 a=x
-                
                 i+=1
             return x
 
 f=Fibonacci(13)
 k=f.run()
 print(k)
+
+
+
+
+#Dans ce cas, on utilise aussi une boucle, à chaque fois on calcule l'élément f(i) et on affecte f(i-1) à a et f(i-2) à b
+#jusqu'à arriver à la fin de la boucle
